8b52e985bf2c893e1012cedb7687ede3b855f81b
smayr <mayr97.sm@gmx.at>
2023-01-07 16:14:00 +0100

    Vitest for LanguageSwitcher implemented

--- huxle/vite.config.ts
+++ huxle/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
@@ -11,4 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  test: {
+    environment: 'jsdom',
+  }
 });
