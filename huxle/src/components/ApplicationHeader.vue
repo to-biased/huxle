1dc00f1b95e0406bff94234e7cb8c82b23b2c6d1
Bledixon Panda <12498599+Bledixon@users.noreply.github.com>
2023-01-07 11:38:39 +0100

    Update ApplicationHeader.vue

--- huxle/src/components/ApplicationHeader.vue
+++ huxle/src/components/ApplicationHeader.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts">
+import LanguageSwitcher from "./LanguageSwitcher.vue";
+</script>
+
+
 <template>
   <header>
     <div class="flex items-center justify-between h-16 w-full drop-shadow-md absolute bg-slate-200">
       <p class="font-bold text-2xl pl-4">Huxle</p>
       <div class="pr-4 font-medium text-lg">
-        <a href="">Deutsch</a>
-        |
-        <a href="">English</a>
+        <LanguageSwitcher />
       </div>
     </div>
   </header>
