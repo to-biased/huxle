970e2870b56e45342092b298f0bb847fb60bbfe4
smayr <mayr97.sm@gmx.at>
2023-01-07 08:43:51 +0100

    i18n installed, LanguageSwitcher Component created, Language Json Files created

--- huxle/src/App.vue
+++ huxle/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
+    <LanguageSwitcher />
+    {{$t('adminForm.title.label')}}
+  </div>
+  <div>
     <GameView />
   </div>
 </template>
 <script setup lang="ts">
 import GameView from "./views/GameView.vue";
+import LanguageSwitcher from "./components/LanguageSwitcher.vue";
 </script>
 <style scoped></style>
