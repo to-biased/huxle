e1ee00b5853c1c3fcf42b1d1353cb4ba7740f5c3
smayr <mayr97.sm@gmx.at>
2023-01-08 19:50:29 +0100

    word-row implemented in Statsmodal

--- huxle/src/components/StatsModal.vue
+++ huxle/src/components/StatsModal.vue
@@ -1,7 +1,16 @@
 <template>
     <h1 class="text-4xl">Stats</h1>
-    <p>Guesses: </p>
-    <p>Time: </p>
+    <p>Guesses: {{ gameStore.getGuessIndex }}</p>
+    <div v-for="(guess, index) in gameStore.getGuesses">{{guess}}</div>
+    <div>
+        <word-row 
+            v-for="(guess, i) in gameStore.getGuesses"
+            :key="i"
+            :value="guess"
+            :prompt="gameStore.getPrompt"
+            :submitted="i < gameStore.getGuessIndex"
+        />
+    </div>
     <button class="bg-blue-500 hover:bg-blue-700 text-white text-sm px-2 py-1 rounded m-4" @click="copyToClipboard">
     Share
     </button>
@@ -9,6 +18,12 @@
 </template>
   
 <script setup lang="ts">
+import GameView from "@/views/GameView.vue";
+import { useGameStore, type GameState } from "../store/game";
+import WordRow from "../components/WordRow.vue";
+
+const gameStore = useGameStore();
+
 const copyToClipboard = async () => {
     const text = props.result.toString();
     try {
